f185151e0fa89e007eaeb49ae1885064f5ff85b9
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2024-08-09 15:01:10 +0530

    portfolio

---
+++ output.js
@@ -0,0 +1,6 @@
+function showOut() {
+  const inputElement = document.querySelector(".input-js").value;
+  console.log(inputElement);
+
+  window.location.assign("https://mail.google.com/mail/u/0/#inbox?compose=new");
+}
